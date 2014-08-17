e5edd3670a55a653e936825222df2205dd302f42
Johan Sveholm <jsveholm@gmail.com>
2014-08-17 21:11:39 +0900

    rewrite new and _form

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,7 +1,6 @@
 module ApplicationHelper
 
   def present(object, klass = nil)
-    object = object.object if object.respond_to?(:object)
     klass ||= "#{object.class}Presenter".constantize
     presenter = klass.new(object, self)
     yield presenter if block_given?
