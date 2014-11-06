0caeeee80ef1df703f1bf924205cfc4ecf70a5bd
Johan Sveholm <jsveholm@gmail.com>
2014-11-06 04:46:00 +0000

    install rbenv and ruby with vagrant

---
+++ rbenv.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+git clone https://github.com/sstephenson/rbenv.git ~/.rbenv
+echo 'export PATH="$HOME/.rbenv/bin:$PATH"' >> ~/.bashrc
+echo 'eval "$(rbenv init -)"' >> ~/.bashrc
+git clone https://github.com/sstephenson/ruby-build.git ~/.rbenv/plugins/ruby-build
+/home/vagrant/.rbenv/bin/rbenv install 2.1.1
+
