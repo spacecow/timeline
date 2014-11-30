6ad07ccd8bbcd10a29413ad0e1e2508593f8afa6
Johan Sveholm <jsveholm@gmail.com>
2014-11-30 14:14:03 +0900

    edit event at show page

--- spec/views/events/show_spec.rb
+++ spec/views/events/show_spec.rb
@@ -1,14 +1,32 @@
-require 'spec_helper'
+require 'view_helper'
 
 describe 'events/show.html.erb' do
-  let(:event){ mock_model(Event).as_null_object }
-  let(:add_articles_form){ AddArticlesForm.new event }
   let(:rendering){ Capybara.string rendered }
+  let(:event){ double :event }
+  let(:form){ double :from, event:event }
   before do
-    assign :add_articles_form, add_articles_form
-    render 
+    assign :add_articles_form, form
+    stub_template "events/_add_articles_form.html.erb" => "<form>form</form>"
+    render
   end
 
-  it{ rendering.should have_selector 'ul.actions' }
-
+  context "div" do
+    subject(:div){ rendering.find 'div.event' }
+    context "header" do
+      subject{ div.find 'h3' }
+      its(:text){ should eq 'The Slaughter' }
+    end
+    context "form" do
+      subject{ div.find 'form' }
+      its(:text){ should eq "form" }
+    end
+    context "actions" do
+      subject(:ul){ div.find 'ul.actions' }
+      context "edit action" do
+        subject{ ul.find 'li.action.edit a' }
+        its(:text){ should eq "Edit" }
+        its([:href]){ should eq edit_event_path(event) }
+      end
+    end
+  end
 end
