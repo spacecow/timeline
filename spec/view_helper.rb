85cffd03339178e4b2e47b6f0e03a5426bbeca2a
Johan Sveholm <ts-johan.sveholm@mail.rakuten.com>
2014-12-03 08:54:21 +0000

    mover things to view_helper

--- spec/view_helper.rb
+++ spec/view_helper.rb
@@ -8,3 +8,31 @@ class ErbBinding
   end
   def get_bindings; binding end
 end
+
+module MyLetDeclarations
+  module MyHelper
+    def link_to lbl, path
+      "<a href=\"#{path}\">#{lbl}</a>"
+    end
+
+    def method_missing(m, *args, &block)  
+      if /(?<action>\w+)_(?<model>\w+)_path/ =~ m
+        "/#{model}s/#{action}?#{args.first.map{|k,v| "#{k}=#{v}"}.join('&')}"
+      end
+    end  
+  end
+
+  extend RSpec::Core::SharedContext
+  let(:rendered){ @erb.result @local_bindings }
+
+  def render(file:nil, locals:{}, helper:nil)
+    erb_binding = ErbBinding.
+      new(locals).
+      extend(MyHelper)
+    erb_binding = erb_binding.extend(helper) unless helper.nil?
+    @local_bindings = erb_binding.instance_eval{binding}
+    @erb = ERB.new File.read(file)
+  end
+
+end
+RSpec.configure { |c| c.include MyLetDeclarations }
